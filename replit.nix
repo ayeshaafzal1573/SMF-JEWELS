9456ba3339ea0746f242d31aa8351af395a76e9e
Ayesha Afzal <113835417+ayeshaafzal1573@users.noreply.github.com>
2025-07-07 14:14:20 +0000

    replit-deployment

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python312
+    pkgs.python312Packages.uvicorn
+    pkgs.python312Packages.fastapi
+    pkgs.python312Packages.pymongo
+    pkgs.python312Packages.motor
+    pkgs.python312Packages.python-dotenv
+    pkgs.python312Packages.email_validator
+    pkgs.python312Packages.passlib 
+    pkgs.python312Packages.python-jose
+
+  ];
+}
